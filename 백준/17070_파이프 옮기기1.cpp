84668ca332364197224f3c9dab5188e4ac55504d
Kimwoojae <50267433+kwj1270@users.noreply.github.com>
2020-10-06 14:54:53 +0900

    Update 17070_파이프 옮기기1.cpp

--- 백준/17070_파이프 옮기기1.cpp
+++ 백준/17070_파이프 옮기기1.cpp
@@ -12,7 +12,46 @@ using namespace std;
 
 int n;
 int map[MAX][MAX];
-int d[DIRECTION][MAX][MAX];
+//pair<int, int> next[] = {{0,1},{1,0},{1,1}};
+
+int go(int x, int y, int dir){
+    if (x == n-1 && y == n-1) {
+        return 1;
+    }
+    
+    int result = 0;
+    if(dir == 0){
+        if(y+1 < n && map[x][y+1] == 0){
+            result += go(x, y+1, 0);
+
+        }
+        if(x+1 < n && y+1 < n && map[x+1][y+1] == 0 && map[x+1][y] == 0 && map[x][y+1] == 0){
+            result += go(x+1, y+1, 2);
+        }
+    }
+    if(dir == 1){
+        if(x+1 < n && map[x+1][y] == 0){
+            result += go(x+1, y, 1);
+        }
+        if(x+1 < n && y+1 < n && map[x+1][y+1] == 0 && map[x+1][y] == 0 && map[x][y+1] == 0){
+            result += go(x+1, y+1, 2);
+        }
+    }
+    if(dir == 2){
+        if(x+1 < n && y+1 < n && map[x+1][y+1] == 0 && map[x+1][y] == 0 && map[x][y+1] == 0){
+            result += go(x+1, y+1, 2);
+        }
+        if(y+1 < n && map[x][y+1] == 0){
+            result += go(x, y+1, 0);
+        }
+        if(x+1 < n && map[x+1][y] == 0){
+            result += go(x+1, y, 1);
+        }
+    }
+    
+    return result;
+}
+
 int main() {
     cin >> n;
     
@@ -23,56 +62,8 @@ int main() {
             cin >> map[i][j];
         }
     }
-    
-    // 0 -> ,1 | ,2 \
-    
-    d[0][0][1] = 1;
-    
-    queue<tuple<int, int, int>> q;
-    q.push(make_tuple(0,0,1));
-    
-    while (!q.empty()) {
-        int dir = get<0>(q.front());
-        int now_x = get<1>(q.front());
-        int now_y = get<2>(q.front());
-        q.pop();
-        if((dir == 0 || dir == 2) && (now_y+1 < n && now_y+1 >= 0)){
-            if(map[now_x][now_y+1] == 1) continue;
-            d[0][now_x][now_y+1]++;
-            q.push(make_tuple(0, now_x, now_y+1));
-        }
-        if((dir == 1 || dir == 2) && (now_x+1 < n && now_x+1 >= 0)){
-            if(map[now_x+1][now_y] == 1) continue;
-            d[1][now_x+1][now_y]++;
-            q.push(make_tuple(1, now_x+1, now_y));
-        }
-        if((now_y+1 < n && now_y+1 >= 0) && (now_x+1 < n && now_x+1 >= 0)){
-            if(map[now_x][now_y+1] == 1) continue;
-            if(map[now_x+1][now_y] == 1) continue;
-            if(map[now_x+1][now_y+1] == 1) continue;
-            
-            d[2][now_x+1][now_y+1]++;
-            q.push(make_tuple(2, now_x+1, now_y+1));
-        }
-    }
-    /*
-    for(int k=0; k < DIRECTION; k++){
-        for(int i=0; i < n; i++){
-            for(int j=0; j < n; j++){
-                cout << d[k][i][j] << " ";
-            }
-            cout << "\n";
-        }
-        cout << "\n\n";
-    }
-    */
-    
-    int answer = 0;
-    for(int k=0; k < DIRECTION; k++){
-        answer += d[k][n-1][n-1];
-    }
-    cout << answer << "\n";
-    
+    cout << go(0,1,0) << "\n";
+
     return 0;
 }
 
