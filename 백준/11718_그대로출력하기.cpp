e90a978bd16b1fc4d99c1df13ef8e8cda6f90e2f
Kimwoojae <50267433+kwj1270@users.noreply.github.com>
2020-11-02 22:58:02 +0900

    Create 11718_그대로출력하기.cpp

---
+++ 백준/11718_그대로출력하기.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int main() {
+    char ch;
+    while (scanf("%c", &ch) != EOF) {
+        printf("%c",ch);
+    }
+    
+    return 0;
+}
+
+
